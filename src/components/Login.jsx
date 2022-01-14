f27b12331582d1698b2b0e061e1e464dd7841f52
ankur598 <ankursingh.as278@gmail.com>
2022-01-14 20:56:53 +0530

    added login comp

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,36 @@
 function Login() {
   return (
     <>
-      <h1>Login</h1>
+      <p>If you are an artist you could login and add you albums here</p>
+      <div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@artist.com"
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">
+            Enter Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="********"
+          />
+        </div>
+        <div className="d-grid gap-2">
+          <button className="btn btn-secondary" type="button">
+            Login
+          </button>
+        </div>
+      </div>
     </>
   );
 }
